e9df344123309db3d28c6cd562f573431af075e7
student <student@GFL-1472.ad.geeksforless.net>
2017-10-27 17:37:25 +0300

    task3

--- task1/app/main.js
+++ task1/app/main.js
@@ -1,4 +1,4 @@
-angular.module("app", []);
+angular.module('app', ['ui.router']);
 angular.module("app").controller('Controller1', Controller1);
 angular.module("app").controller('Controller2', Controller2);
 
